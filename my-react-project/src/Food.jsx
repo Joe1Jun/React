a650bde89d047efc66afabd8188e5535c0d5cba1
Joe1Jun <junker91joe@gmail.com>
2024-08-06 12:00:29 +0200

    Basic creation of components and using javascript variables inside return statements

---
+++ my-react-project/src/Food.jsx
@@ -0,0 +1,29 @@
+
+
+
+
+function Food() {
+    
+    const food1 = "Orange";
+    const food2 = "Banana";
+    
+    
+    
+    
+    // adding javascript code in return statement you need curly braces.
+    return (
+
+        <ul>
+            <li>Apple</li>
+            <li>{food1 }</li>
+            <li>{food2 }</li>
+       </ul>
+
+    );
+
+
+
+}
+
+
+export default Food
